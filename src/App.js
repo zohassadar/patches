0f54bbd6a57e99b54d3598ab5b01d8d615ad0a18
zohassadar <zohassadar@gmail.com>
2024-05-23 03:20:54 +0000

    Show md5sum of file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import './App.css';
+import { useState } from 'react';
+
+const CryptoJS = require('crypto-js');
+
+function RomInformation({ information }) {
+    return (
+        <>
+            <p>{information}</p>
+        </>
+    );
+}
+
+function RomInput({ setInformation }) {
+    function handleInput(event) {
+        const reader = new FileReader();
+        reader.readAsBinaryString(event.target.files[0]);
+
+        reader.onload = (event) => {
+            console.log(`Read ${event.target.result.length} bytes`);
+            const contents = event.target.result;
+            const hash = CryptoJS.MD5(
+                CryptoJS.enc.Latin1.parse(contents),
+            ).toString();
+            setInformation(hash);
+        };
+    }
+    return (
+        <>
+            <input name="romUpload" type="file" onInput={handleInput} />
+        </>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This line of text has been edited
-        </p>
-      </header>
-    </div>
-  );
+    const [information, setInformation] = useState('');
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>get md5</p>
+                <RomInput setInformation={setInformation} />
+                <RomInformation information={information} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
