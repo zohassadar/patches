571a7eae708889d15a114a9be965c6387bbbb162
zohassadar <zohassadar@gmail.com>
2024-05-24 00:48:56 +0000

    Details for patch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 const { saveAs, MarcFile, md5, parseBPSFile } = require('./bps.js');
 
+const INES1HEADER = [78, 69, 83, 26, 2, 2, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function Information({ hide, information }) {
     if (hide) return;
     return (
@@ -63,32 +65,37 @@ function FileInput({ name, handleInput, hide }) {
 function App() {
     const [rom, setRom] = useState(null);
     const [patched, setPatched] = useState(null);
-
     const [romInfo, setRomInfo] = useState('');
     function handleRomInput(romFile) {
-        const romMarc = new MarcFile(romFile.target.files[0], onMarcRomLoad);
+        var romMarc = new MarcFile(romFile.target.files[0], onMarcRomLoad);
         function onMarcRomLoad() {
+            romMarc = new MarcFile(
+                new Uint8Array([...INES1HEADER, ...romMarc._u8array.slice(16)]),
+            );
             setRom({
                 filename: romFile.target.files[0].name,
                 contents: romMarc,
             });
             const hash = md5(romMarc._u8array).toString();
             setRomInfo(hash);
-            fetch('patches/tetris.bps')
+            fetch('patches/TetrisGYM-6.0.0.bps')
                 .then((response) => response.blob())
                 .then((patchData) => {
                     patchData.arrayBuffer().then((buffer) => {
                         try {
-                            const marcPatch = new MarcFile(new Uint8Array(buffer));
+                            const marcPatch = new MarcFile(
+                                new Uint8Array(buffer),
+                            );
                             const patchParsed = parseBPSFile(marcPatch);
                             const patchedRom = patchParsed.apply(romMarc, true);
                             setPatched({
-                                filename: 'patched.nes',
+                                filename: 'TetrisGYM-6.0.0.nes',
                                 contents: patchedRom,
                                 valid: true,
                             });
-                        } catch {
-                            debugger;
+                        } catch (exc) {
+                            setRomInfo('invalid rom');
+                            setPatched({valid: false})
                         }
                     });
                 });
@@ -101,11 +108,6 @@ function App() {
                 <Information hide={false} information="give rom" />
                 <NewFileInput name="RomInput" handleInput={handleRomInput} />
                 <Information hide={!rom} information={romInfo} />
-                <SaveFile
-                    hide={!rom}
-                    file={rom}
-                    text="download unmodified rom"
-                />
                 <SaveFile
                     hide={!rom || (patched && !patched.valid)}
                     file={patched}
