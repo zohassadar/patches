bd3bc6785c55b3afa70fd39ec1ab83d02c71e45e
zohassadar <zohassadar@gmail.com>
2024-05-25 01:29:41 +0000

    Multiple patches available (with colorful boxes)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import { useState } from 'react';
-
-const { saveAs, MarcFile, parseBPSFile } = require('./bps.js');
+import { patches } from './patches.js';
+const {
+    saveAs,
+    MarcFile,
+    parseBPSFile,
+    parseIPSFile,
+    md5,
+} = require('./bps.js');
 
 const INES1HEADER = [78, 69, 83, 26, 2, 2, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const VANILLA_INES1_MD5 = 'ec58574d96bee8c8927884ae6e7a2508';
 
 function Information({ hide, information }) {
     if (hide) return;
-    return (
-        <div className="info">
-            <p>{information}</p>
-        </div>
-    );
+    return <p className="info">{information}</p>;
 }
 
-function SaveFile({ hide, file, text }) {
-    if (hide) return;
-    function downloadRom() {
-        saveAs(new Blob([file.contents._u8array]), file.filename);
+function SaveFile({ rom, patch }) {
+    if (!rom) return;
+    function downloadRom(data, name) {
+        patchSomething(patch, rom);
     }
     return (
         <div className="download">
-            <button onClick={downloadRom}>{text}</button>
+            <button onClick={downloadRom}>Download patched</button>
         </div>
     );
 }
@@ -34,91 +37,114 @@ function NewFileInput({ name, handleInput, hide }) {
         </div>
     );
 }
-/* eslint-disable */
-function FileInput({ name, handleInput, hide }) {
-    if (hide) return;
-    function handleFileLoad(fileEvent, filename) {
-        console.log(`Read ${fileEvent.target.result.length} bytes`);
-        const contents = fileEvent.target.result;
-        const bytes = new Uint8Array(contents.length);
-        for (let i = 0; i < contents.length; i++) {
-            bytes[i] = contents[i].charCodeAt(0);
-        }
-        handleInput({ filename: filename, contents: bytes });
-    }
 
-    function _handleInput(event) {
-        const filename = event.target.files[0].name;
-        const reader = new FileReader();
-        reader.readAsBinaryString(event.target.files[0]);
-        reader.onload = (fileEvent) => handleFileLoad(fileEvent, filename);
-    }
+function YouTube({ vid }) {
+    if (!vid) return;
+    return (
+        <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${vid}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+        ></iframe>
+    );
+}
+
+function Table({ patch, rom }) {
+    if (!patch) return;
     return (
         <>
-            <input name={name} type="file" onInput={_handleInput} />
+            <h3>{patch.name}</h3>
+            <p>{patch.desc}</p>
+            <p>{patch.authors.join(', ')}</p>
+            <p>
+                <a href={`/patches/${patch.file}`}>download patch</a>
+            </p>
+            <YouTube vid={patch.yt} />
+            <SaveFile text="download patched" rom={rom} patch={patch} />
+        </>
+    );
+}
+function SideNames({ setPatch }) {
+    return (
+        <>
+            {patches.map((patch) => {
+                return <p onClick={() => setPatch(patch)}>{patch.name}</p>;
+            })}
         </>
     );
 }
 
-/* eslint-enable */
+function patchSomething(patch, rom) {
+    const bpsTest = new RegExp(/\.bps$/);
+    fetch(`patches/${patch.file}`)
+        .then((response) => response.blob())
+        .then((patchData) => {
+            patchData.arrayBuffer().then((buffer) => {
+                const marcPatch = new MarcFile(new Uint8Array(buffer));
+                let patchParsed;
+                if (bpsTest.test(patch.file)) {
+                    console.log(patch.file);
+                    patchParsed = parseBPSFile(marcPatch);
+                } else {
+                    console.log('IPS file');
+                    patchParsed = parseIPSFile(marcPatch);
+                }
+
+                const patchedRom = patchParsed.apply(rom.contents, true);
+                saveAs(new Blob([patchedRom._u8array]), 'patched.nes');
+            });
+        });
+}
 
 function App() {
     const [rom, setRom] = useState(null);
     const [patched, setPatched] = useState(null);
     const [romInfo, setRomInfo] = useState('Waiting for Rom');
+
+    const [patch, setPatch] = useState(null);
+
     function handleRomInput(romFile) {
         var romMarc = new MarcFile(romFile.target.files[0], onMarcRomLoad);
         function onMarcRomLoad() {
             romMarc = new MarcFile(
                 new Uint8Array([...INES1HEADER, ...romMarc._u8array.slice(16)]),
             );
-            setRom({
-                filename: romFile.target.files[0].name,
-                contents: romMarc,
-            });
-            // const hash = md5(romMarc._u8array).toString();
-            fetch('patches/TetrisGYM-6.0.0.bps')
-                .then((response) => response.blob())
-                .then((patchData) => {
-                    patchData.arrayBuffer().then((buffer) => {
-                        try {
-                            const marcPatch = new MarcFile(
-                                new Uint8Array(buffer),
-                            );
-                            const patchParsed = parseBPSFile(marcPatch);
-                            const patchedRom = patchParsed.apply(romMarc, true);
-                            setRomInfo('this rom will work!');
-                            setPatched({
-                                filename: 'TetrisGYM-6.0.0.nes',
-                                contents: patchedRom,
-                                valid: true,
-                            });
-                        } catch (exc) {
-                            setRomInfo('This is an invalid rom');
-                            setPatched({ valid: false });
-                        }
-                    });
+            const hash = md5(romMarc._u8array).toString();
+            if (hash == VANILLA_INES1_MD5) {
+                setRomInfo('This rom will work!');
+                setRom({
+                    filename: romFile.target.files[0].name,
+                    contents: romMarc,
                 });
+            } else {
+                setRomInfo('Unknown rom');
+            }
         }
     }
 
     return (
         <div className="App">
-            <header className="App-header">
-                <div className="headerBox">
-                    <Information hide={false} information="give rom" />
-                    <NewFileInput
-                        name="RomInput"
-                        handleInput={handleRomInput}
-                    />
-                    <Information information={romInfo} />
-                </div>
-                <SaveFile
-                    hide={!rom || (patched && !patched.valid)}
-                    file={patched}
-                    text="download patched"
-                />
+            <header className="headerBox">
+                <Information information="Patch tetris.nes" />
             </header>
+            <div className="topBox">
+                <Information hide={false} information="give rom" />
+                <NewFileInput name="RomInput" handleInput={handleRomInput} />
+                <Information information={romInfo} />
+            </div>
+            <div className="bottomBox">
+                <div className="sideBox">
+                    <SideNames setPatch={setPatch} />
+                </div>
+                <div className="tableBox">
+                    <Table patch={patch} rom={rom} />
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/patches.js
@@ -0,0 +1,22 @@
+export const patches = [
+    {
+        "name": "Tetris GYM 6.0.0",
+        "file": "TetrisGYM-6.0.0.bps",
+        "desc": "Adds practice modes to NES Tetris (among other things)",
+        "yt": "9DCRISvw5vs",
+        "authors": ["kirjava"]
+    },
+    {
+        "name": "Tetris GYM 6.0.0 Tron hack",
+        "file": "TetrisGYM-6.0.0-tron0.bps",
+        "desc": "Adds an extra dark mode to Tetris GYM",
+        "authors": ["kirjava","zohassadar"],
+        "yt": ""
+    },
+    {
+        "name": "tetris next box playfield",
+        "file": "tetris_next_box_playfield.ips",
+        "desc": "Play tetris in the next box",
+        "authors": ["maya"]
+    }
+]
