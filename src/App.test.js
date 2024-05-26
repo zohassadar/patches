b1c46e8832fb0b7c0c843d2b8a65d2697a13cfbf
zohassadar <zohassadar@gmail.com>
2024-05-26 06:25:21 +0000

    Remove unused

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders edited text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/has been edited/i);
-  expect(linkElement).toBeInTheDocument();
-});
